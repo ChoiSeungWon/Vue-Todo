a626f730c6d205ff3b1aa7b98c1dea0e91a1fa27
ChoiSeungWon <b088081@gmail.com>
2019-02-07 14:54:55 +0900

    2019.02.07 vue.js todo project

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <input type="text" name="calendar" id="calendar">
+</template>
+
+<script>
+export default {
+  mounted() {
+    $('#calendar').datepicker();
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
